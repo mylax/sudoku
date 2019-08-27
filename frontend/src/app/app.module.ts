540784f963fe2d4b7b3f1ae0a1381992069f69ee
Florian Schwarz <Florian.Schwarz@de.ey.com>
2019-08-27 10:55:45 +0200

    reworked css and html, now board looks good enough, time to work on other things

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { HeaderComponent } from './header/header.component';
+import { MenuRightComponent } from './menu-right/menu-right.component';
+import { MenuLeftComponent } from './menu-left/menu-left.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuRightComponent,
+    MenuLeftComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/board/board.component.ts
+++ frontend/src/app/board/board.component.ts
@@ -9,35 +9,27 @@ import { HttpClient } from '@angular/common/http';
 export class BoardComponent implements OnInit {
   rows: number[];
   columns: number[];
-  ids: number[];
-  val: number[];
+  data: any;
 
   constructor(private httpClient: HttpClient) {
     this.rows = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     this.columns = [1, 2, 3];
-    this.ids = [];
-    this.val = [];
+    this.data = [];
     this.httpClient.get("http://localhost:6002/get_game_start").subscribe((data)=>{
-      for(var i in data){
-        this.ids.push(Number(i));
-        this.val.push(data[i]);
-      }
-      console.log(this.ids);
-      console.log(this.val);
-
-    });
+      this.data = data;
+      console.log(data);
+      });
    }
 
   find_id(row, column) {
     return column + (row-1) * 9;
   }
+
   find_val(row, column) {
-    var id = this.find_id(row, column);
-    var id_of_val = this.ids.indexOf(id);
-    if (id_of_val !== -1) {
-      return this.val[id_of_val];
-    }
-    return ""
+    let id = this.find_id(row, column);
+    let val = this.data[id];
+    if (val) { return val};
+    return "";
   }
   ngOnInit() {
   }
